357ab79f295ecccd31c2ca2021bdfe0fa25b41ff
alenck <wjdgkdusjeong@naver.com>
2024-11-21 21:19:50 +0900

    second commit

---
+++ app/src/main/java/com/example/calculate/fragment/pastCalculateFragment.kt
@@ -0,0 +1,22 @@
+package com.example.calculate.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.calculate.R
+
+class pastCalculateFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_past_calculate, container, false)
+    }
+}
