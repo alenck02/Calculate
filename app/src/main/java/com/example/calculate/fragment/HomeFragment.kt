d3417dc6e8d4620ae5b6b868017d7cb1fdaaac17
alenck <wjdgkdusjeong@naver.com>
2024-12-18 23:56:42 +0900

    commit

--- app/src/main/java/com/example/calculate/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/calculate/fragment/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ScrollView
 import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.navigation.NavOptions
